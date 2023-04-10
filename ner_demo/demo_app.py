284415a6db7ce2b49d4462f717153d8ed4d6a79e
Dwijen Chawra <dchawra@purdue.edu>
2023-04-10 14:21:17 -0400

    changed colors

--- ner_demo/demo_app.py
+++ ner_demo/demo_app.py
@@ -190,15 +190,38 @@ def download(filename):
         print("<p>Rendering visualization</p>")
 
         colors = {}
+        # for label in labels:
+        #     print(label)
+        #     if label[0] == "B":
+        #         colors[label] = "rgb(94, 164, 80)"
+        #     elif label[0] == "I":
+        #         colors[label] = "rgb(152, 177, 207)"
+        #     elif label[0] == "L":
+        #         colors[label] = "rgb(179, 98, 96)"
+        #     elif label[0] == "U":
+        #         colors[label] = "rgb(220, 212, 126)"
+
         for label in labels:
-            if label[0] == "B":
+            if label[2:] == "ADE":
                 colors[label] = "rgb(94, 164, 80)"
-            elif label[0] == "I":
-                colors[label] = "rgb(152, 177, 207)"
-            elif label[0] == "L":
-                colors[label] = "rgb(179, 98, 96)"
-            elif label[0] == "U":
-                colors[label] = "rgb(220, 212, 126)"
+            elif label[2:] == "Dosage":
+                colors[label] = "rgb(230, 126, 34)"
+            elif label[2:] == "Drug":
+                colors[label] = "rgb(52, 152, 219)"
+            elif label[2:] == "Duration":
+                colors[label] = "rgb(46, 204, 113)"
+            elif label[2:] == "Form":
+                colors[label] = "rgb(155, 89, 182)"
+            elif label[2:] == "Frequency":
+                colors[label] = "rgb(241, 196, 15)"
+            elif label[2:] == "Reason":
+                colors[label] = "rgb(211, 84, 0)"
+            elif label[2:] == "Route":
+                colors[label] = "rgb(149, 165, 166)"
+            elif label[2:] == "Strength":
+                colors[label] = "rgb(230, 126, 34)"
+        
+
 
 
             
@@ -216,85 +239,86 @@ def download(filename):
         resp.headers['Content-Type'] = 'text/html'
         return resp
 
-# @app.route('/streamnlp/<path:filename>')
-# def newfunction(filename):
-#     def process1():
-#         file = filename
+@app.route('/streamnlp/<path:filename>')
+def newfunction(filename):
+    def process1():
+        file = filename
 
-#         text = ""
-#         lines = []
-#         processedlines = []
-#         yield "Starting processing"
-#         with open(os.path.join("../data/ner_data_formatted/txt/", file)) as f:
-#             for line in f:
-#                 text += line
-#                 lines.append(line)
-#                 processedlines.append(nlp(line))
+        text = ""
+        lines = []
+        processedlines = []
+        yield "<p>Starting processing</p>"
+        with open(os.path.join("../data/ner_data_formatted/txt/", file)) as f:
+            for line in f:
+                text += line
+                lines.append(line)
+                processedlines.append(nlp(line))
         
-#         yield "Processing complete"
+        yield "Processing complete"
 
-#         zipped = zip(lines, processedlines)
+        zipped = zip(lines, processedlines)
 
-#         #process syllables
-#         combinedlines = []
+        #process syllables
+        combinedlines = []
 
-#         yield "<p>Processing syllables</p>"
+        yield "<p>Processing syllables</p>"
 
-#         currlen = 0
-#         for item in zipped:
-#             processed = item[1]
-#             combined = []
-#             for i in range(len(processed)):
-#                 if processed[i]["word"].startswith('##'):
-#                     continue
-#                 # Otherwise, combine it with the next string if it starts with "##"
-#                 word = processed[i]["word"]
-#                 start = processed[i]["start"]
-#                 end = processed[i]["end"]
-#                 for j in range(i+1, len(processed)):
-#                     if processed[j]["word"].startswith('##'):
-#                         word += processed[j]["word"][2:]
-#                         end = processed[j]["end"]
-#                     else:
-#                         break
-#                 # consider the previous end
-#                 combined.append({"word": word, "entity": processed[i]["entity"], "start": start + currlen, "end": end + currlen})
-#             currlen += len(item[0])
-#             combinedlines.extend(combined)
+        currlen = 0
+        for item in zipped:
+            processed = item[1]
+            combined = []
+            for i in range(len(processed)):
+                if processed[i]["word"].startswith('##'):
+                    continue
+                # Otherwise, combine it with the next string if it starts with "##"
+                word = processed[i]["word"]
+                start = processed[i]["start"]
+                end = processed[i]["end"]
+                for j in range(i+1, len(processed)):
+                    if processed[j]["word"].startswith('##'):
+                        word += processed[j]["word"][2:]
+                        end = processed[j]["end"]
+                    else:
+                        break
+                # consider the previous end
+                combined.append({"word": word, "entity": processed[i]["entity"], "start": start + currlen, "end": end + currlen})
+            currlen += len(item[0])
+            combinedlines.extend(combined)
         
-#         yield "<p>Syllables processed</p>"
+        yield "<p>Syllables processed</p>"
 
-#         for i in range(len(combinedlines)):
-#             # example output: {'end': None, 'entity': 'LABEL_27', 'index': 131, 'score': 0.9996716, 'start': None, 'word': 'and'}
-#             combinedlines[i]["label"] = labels[int(combinedlines[i]["entity"].split("_")[1])]
-#             # if i == 0:
-#             #     combined[i]["start"] = 0
-#             #     combined[i]["end"] = len(combined[i]["word"])
-#             # else:
-#             #     combined[i]["start"] = combined[i-1]["end"] + 2
-#             #     combined[i]["end"] = combined[i]["start"] + len(combined[i]["word"])
+        for i in range(len(combinedlines)):
+            # example output: {'end': None, 'entity': 'LABEL_27', 'index': 131, 'score': 0.9996716, 'start': None, 'word': 'and'}
+            combinedlines[i]["label"] = labels[int(combinedlines[i]["entity"].split("_")[1])]
+            # if i == 0:
+            #     combined[i]["start"] = 0
+            #     combined[i]["end"] = len(combined[i]["word"])
+            # else:
+            #     combined[i]["start"] = combined[i-1]["end"] + 2
+            #     combined[i]["end"] = combined[i]["start"] + len(combined[i]["word"])
 
-#         # pprint(combinedlines[102:150])
+        # pprint(combinedlines[102:150])
             
-#         # Generate the visualization using displacy module
-#         # options = {"ents": labels}
+        # Generate the visualization using displacy module
+        # options = {"ents": labels}
 
-#         yield "<p>Rendering visualization</p>"
+        yield "<p>Rendering visualization</p>"
 
-#         options = {"ents": [ent for ent in labels if ent != "O"]}
+        options = {"ents": [ent for ent in labels if ent != "O"]}
 
-#         # print("len of ents")
-#         # print(len(options["ents"]))
-#         # doc = {"text": text, "ents": [{"start": i["start"], "end": i["end"], "label": i["label"]} for i in combinedlines]}
-#         doc = {"text": text, "ents": [{"start": i["start"], "end": i["end"], "label": i["label"]} for i in combinedlines if i["label"] != "O"]}
+        # print("len of ents")
+        # print(len(options["ents"]))
+        # doc = {"text": text, "ents": [{"start": i["start"], "end": i["end"], "label": i["label"]} for i in combinedlines]}
+        doc = {"text": text, "ents": [{"start": i["start"], "end": i["end"], "label": i["label"]} for i in combinedlines if i["label"] != "O"]}
 
-#         html = displacy.render(doc, style="ent", options=options, manual=True)
+        html = displacy.render(doc, style="ent", options=options, manual=True)
         
-#         with app.app_context():
-#             resp = make_response(html)
-#             resp.headers['Content-Type'] = 'text/html'
-#             yield resp
-#     return Response(process1(), mimetype='text/html')
+        with app.app_context():
+            resp = make_response(html)
+            resp.headers['Content-Type'] = 'text/html'
+            # yield resp
+            yield html
+    return Response(process1(), mimetype='text/html')
 
 
 @app.route('/test')
